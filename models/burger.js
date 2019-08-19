aeac8d5273f1075892749d92e9a590b5eb767b3b
Calli Crase <callilogancrase@gmail.com>
2019-08-18 18:50:27 -0600

    when devour is clicked, the burger moves to column

--- models/burger.js
+++ models/burger.js
@@ -6,18 +6,19 @@ var burger = {
     orm.selectAll("burgers", function(res) {
       cb(res);
     });
-  }//,
+  },
   // The variables cols and vals are arrays.
 //   create: function(cols, vals, cb) {
 //     orm.create("cats", cols, vals, function(res) {
 //       cb(res);
 //     });
 //   },
-//   update: function(objColVals, condition, cb) {
-//     orm.update("cats", objColVals, condition, function(res) {
-//       cb(res);
-//     });
-//   },
+update: function(objColVals, condition, cb) {
+  orm.update("burgers", objColVals, condition, function(res) {
+    cb(res);
+  });
+}
+//,
 //   delete: function(condition, cb) {
 //     orm.delete("cats", condition, function(res) {
 //       cb(res);
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,16 +1,9 @@
+// $(function(){
+//     //MOVE BURGER TO DEVOURED COLUMN
+//     $(".change-devoured").on("click", function(event) {
+//         var id = $(this).data("id");
+//         var newDevoured = $(this).data("newdevoured");
+//         console.log(id, newDevoured);
+//       });
 
-    //CREATE A NEW BURGER
-    // $(".create-form").on("submit", function(event) {
-    //     // Make sure to preventDefault on a submit event.
-    //     event.preventDefault();
-    //   });
-
-    //MOVE BURGER TO DEVOURED COLUMN
-    $('.change-devoured').on("click", function(event){
-        alert("you made it")
-
-
-        var id = $(this).data("id");
-        var newDevoured = $(this).data("newdevoured");
-        console.log(id, newDevoured);
-    })
+// });
