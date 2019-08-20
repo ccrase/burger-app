8b345cec4eb711c73f66f701f0121225c8bf981a
Calli Crase <callilogancrase@gmail.com>
2019-08-19 22:33:20 -0600

    added JAWSDB add in

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql');
 
 //Set up SQL connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: 'burgers_db'
-});
+var connection;
+
+if(process.env.JAWSDB){
+    connection= mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: 'burgers_db'
+    }); 
+};
 
 //Make a connection
 connection.connect(function(err){
